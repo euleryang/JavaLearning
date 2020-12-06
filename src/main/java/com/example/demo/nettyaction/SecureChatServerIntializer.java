0f28d544f2eb30afcc0cf87f6c8736e9ea942e71
Sam <14162376@qq.com>
2020-12-06 17:18:43 +0800

    nettyaction-secureChatServer

---
+++ src/main/java/com/example/demo/nettyaction/SecureChatServerIntializer.java
@@ -0,0 +1,32 @@
+package com.example.demo.nettyaction;
+
+import javax.net.ssl.SSLEngine;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class SecureChatServerIntializer extends ChatServerInitializer {    //1
+    private final SslContext context;
+
+    public SecureChatServerIntializer(ChannelGroup group, SslContext context) {
+        super(group);
+        this.context = context;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        super.initChannel(ch);
+        SSLEngine engine = context.newEngine(ch.alloc());
+        engine.setUseClientMode(false);
+        ch.pipeline().addFirst(new SslHandler(engine)); //2
+    }
+}
